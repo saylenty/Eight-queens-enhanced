df57252c9dc05c5f46b4697831303a4a47dc5450
Alexander Druzhynin <alexander.druzhynin@javera.software>
2019-09-19 18:29:18 +0300

    Use build-in Comparator capabilities for Space objects

--- src/main/java/com/gitlab/saylenty/chessPl/gameItems/Space.java
+++ src/main/java/com/gitlab/saylenty/chessPl/gameItems/Space.java
@@ -7,6 +7,8 @@ package com.gitlab.saylenty.chessPl.gameItems;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.Comparator;
+
 /**
  * Space class for the ChessGame
  */
@@ -15,10 +17,11 @@ import lombok.ToString;
 public final class Space implements Comparable<Space> {
     private final int x;
     private final int y;
+    private static final Comparator<Space> spaceComparator =
+            Comparator.comparingInt(Space::getX).thenComparingInt(Space::getY);
 
     @Override
     public int compareTo(Space o) {
-        int cmp = Integer.compare(this.x, o.x);
-        return cmp == 0 ? Integer.compare(this.y, o.y) : cmp;
+        return spaceComparator.compare(this, o);
     }
 }
