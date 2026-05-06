b48743c314a5d17a46331b7d2c337c48febfbc92
saylenty <druzhyninalex@gmail.com>
2026-05-07 01:14:54 +0300

    refactor: replace Lombok in GamePiecesFactory, apply var

--- src/main/java/com/gitlab/saylenty/chessPl/gameItems/GamePiecesFactory.java
+++ src/main/java/com/gitlab/saylenty/chessPl/gameItems/GamePiecesFactory.java
@@ -5,37 +5,26 @@
 package com.gitlab.saylenty.chessPl.gameItems;
 
 import com.gitlab.saylenty.chessPl.gameItems.pieces.Piece;
-import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-/**
- * Factory for pieces creation
- */
-@Log4j2
 public class GamePiecesFactory {
 
-    /**
-     * Creates a game piece and sets it on the board
-     *
-     * @param clazz a piece class
-     * @param color a piece color
-     * @param board a board to associate the piece with
-     * @return created piece
-     */
+    private static final Logger log = LogManager.getLogger(GamePiecesFactory.class);
+
     public <T extends Piece> T createPiece(Class<T> clazz, Piece.Color color, ChessBoard board) {
-        T t;
         try {
-            Constructor<T> constructor = clazz.getConstructor(Piece.Color.class);
-            t = constructor.newInstance(color);
+            var constructor = clazz.getConstructor(Piece.Color.class);
+            var t = constructor.newInstance(color);
             t.setChessBoard(board);
+            return t;
         } catch (InstantiationException | IllegalAccessException |
                 NoSuchMethodException | InvocationTargetException e) {
             log.error(e);
             throw new RuntimeException(
                     String.format("Could not instantiate desired figure of %s class", clazz.getName()));
         }
-        return t;
     }
 }
